99f87ba481dc158cf3599fc23c7de5a627f70313
hung.ai <hung.dhk@hahalolo.com>
2023-03-27 18:04:38 +0700

    update

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request
-from flask.ext.mysql import MySQL
-from werkzeug import generate_password_hash, check_password_hash
+import mysql.connector
+import boto3
+#from flask.ext.mysql import MySQL
+
+#from werkzeug import generate_password_hash, check_password_hash
 
 
 import json
 
 app = Flask(__name__)
-mysql = MySQL()
-# MySQL configurations 
-app.config['MYSQL_DATABASE_USER'] = 'jay'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
-app.config['MYSQL_DATABASE_DB'] = 'BucketList'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mydb = mysql.connector.connect(
+  host="db-hungai.cqv94rykczmh.us-east-1.rds.amazonaws.com",
+  user="admin",
+  password="kieuhung123",
+  database='users'
+)
 
 @app.route("/")
 def main():
@@ -27,22 +30,22 @@ def signUp():
     _name = request.form['inputName']
     _email = request.form['inputEmail']
     _password = request.form['inputPassword']
+    _image = request.form['inputImage']
     # validate the received values 
-    if _name and _email and _password:
-        mysql.init_app(app)
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        _hashed_password = generate_password_hash(_password)
-        cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-        data = cursor.fetchall()
-        if len(data) is 0:
-            conn.commit()
-            return json.dumps({'message':'User created successfully !'})
-        else:
-            return json.dumps({'error':str(data[0])})
+    if _name and _email and _password and _image:
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO user (username, password, email) VALUES (%s, %s, %s)"
+        val = (_name, _password, _email)
+        mycursor.execute(sql, val)
+        mydb.commit()
+
+        s3 = boto3.resource('s3')
+        # Upload a new file
+        #data = open('test.jpg', 'rb')
+        file = request.files['file']
+        s3.Bucket('image-hung-001').put_object(Key=file.filename, Body=_image)
     else:
         return json.dumps({'html':'<span>Enter the required fields</span>'})
-
 
 if __name__ == "__main__":
     app.run()
